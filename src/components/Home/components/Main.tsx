345540f6fe72d4c2168398b3d52423b3225427d9
Yash Arora <yasharora0606@gmail.com>
2022-11-13 23:45:11 -0500

    Update Main.tsx

--- src/components/Home/components/Main.tsx
+++ src/components/Home/components/Main.tsx
@@ -38,7 +38,7 @@ const Main = () => {
                         <p>
                             {"I'm currently a software engineering student at the University of "}
                             <a className="styled-link waterloo hoverable" href="https://uwaterloo.ca" target="_blank" rel="noreferrer">{"Waterloo"}</a>, 
-                            {" working at "}
+                            {". This winter I'm working at "}
                             <a className="styled-link jane-street hoverable" href="https://janestreet.com" target="_blank" rel="noreferrer">{"Jane Street"}</a>
                             {" in NYC. I used to work at "}
                             <a className="styled-link bloomberg hoverable" href="https://bloomberg.com" target="_blank" rel="noreferrer">{"Bloomberg"}</a>
