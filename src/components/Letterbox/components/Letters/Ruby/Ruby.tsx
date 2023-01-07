d8114aeacdb16491ef563dca315e8e707cb089c3
Yash Arora <yasharora0606@gmail.com>
2023-01-07 17:57:43 -0500

    ruby

--- src/components/Letterbox/components/Letters/Ruby/Ruby.tsx
+++ src/components/Letterbox/components/Letters/Ruby/Ruby.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import ChangingText from "../../ChangingText";
 
 const Ruby = () => {
     return (
-        <div className="setting">
-            <div className="letter">
-                <p>
-                    testing
-                    <ChangingText texts={["one", "two"]}/>
-                </p>    
-            </div>
-        </div>
+        // <div className="setting">
+        //     <div className="letter">
+        //         <p>
+        //             testing
+        //             <ChangingText texts={["one", "two"]}/>
+        //         </p>    
+        //     </div>
+        // </div>
+        <Redirect to="/ruby.pdf" />
     );
 };
 
