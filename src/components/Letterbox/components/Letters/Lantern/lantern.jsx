e9db085871158e1c0685d3787d5a03b4f3c8a854
Yash Arora <yasharora0606@gmail.com>
2023-07-10 00:43:02 -0400

    Update lantern.jsx

--- src/components/Letterbox/components/Letters/Lantern/lantern.jsx
+++ src/components/Letterbox/components/Letters/Lantern/lantern.jsx
@@ -13,29 +13,29 @@ const Lantern = () => {
                 </p>
                 <p>
                     I struggle to act as though you are nothing to me when you were so recently everything.
-                    I want to know that it hurt you as much as it did me.
-                    But at the end of the day I hope that life brings you the fulfillment that you deserve, even if that life is one from which I am absent.
+                    Paradoxically, I want to know that it hurt for you as much as it did for me, yet I never want you to feel the pain I felt.
+                    And at the end of the day I pray that our lives, be them separate, continue to bring us love.
                     A piece of my heart will forever ache at the thought of you.
+                    I still wonder how you feel and hope that a piece of your heart aches too.
                 </p>    
                 <p>
-                    I still wonder how you feel and hope that a piece of your heart aches too.
-                    Maybe this feeling will fade with time; it's sickening that you cannot speed up the process.
+                    Maybe this feeling will fade with time, though it's sickening that you can't speed up the process.
                     Hopefully after anough teary eyed nights we will think of each other and smile.
                     We will focus on the memories we made, the time we shared, and the love we gave.
+                    You taught me that nothing in this life is a guarantee.
                 </p>
                 <p>
-                    You taught be that nothing in this life is a guarantee.
                     If I had known that night would be the last time I'd see you, I would have held you tighter.
                     I would have kissed you longer.
                     I would have called you baby or darling or something cringier one more time.
                     I would have said one more "I love you" and "goodbye."
-                    But those opportunities are gone, as are you.
+                    I would have told you how proud I am of you, how beautiful of a soul you have, and how grateful I will forever be to have had you in my life.
+                    But while those opportunities are gone, as are you, the least I can do is tell you how I feel.
                 </p>
-                <img src={lantern} alt="lantern">
-                </img>                
+                <img src={lantern} style={{width: "400px", height: "500px"}} alt="lantern"/>             
                 <p>___</p>
                 <p>
-                    May our lives, be them separate, bring us peace, love, and happiness.
+                    As I let this water lantern meander downstream, I am reminded that our river never stops flowing.
                 </p>
                 <p>
                     Yash.
