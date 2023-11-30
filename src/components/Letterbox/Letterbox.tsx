68022d3b62d24ca4eafd0e2cf90c0564dbf8cfa1
Yash Arora <yasharora0606@gmail.com>
2023-11-30 13:53:05 +0800

    loopy

--- src/components/Letterbox/Letterbox.tsx
+++ src/components/Letterbox/Letterbox.tsx
@@ -6,6 +6,7 @@ import Ruby from "./components/Letters/Ruby/Ruby";
 import Emerald from "./components/Letters/Emerald/emerald";
 import Lantern from "./components/Letters/Lantern/lantern";
 import Jade from "./components/Letters/Jade/jade";
+import Loopy from "./components/Letters/Loopy/loopy";
 
 const Letters = () => {
 
@@ -16,7 +17,8 @@ const Letters = () => {
         "ruby": <Ruby/>,
         "emerald" : <Emerald/>,
         "lantern" : <Lantern/>,
-        "jade" : <Jade/>
+        "jade" : <Jade/>,
+        "loopy": <Loopy/>
     }
 
     const getLetterOrLockScreen = () => {
---
+++ src/components/Letterbox/components/Letters/Loopy/loopy.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Loopy = () => {
+
+    return  (
+        <div className="setting">
+            <div className="letter">
+
+                <p className="title">
+                   Christina,
+                </p>
+                <p>
+                    A couple months ago I was a little bit disappointed in how my exchange term was panning out. I knew that arriving several weeks late would mean that I would miss the orientation week, and that the majority of exchange students find their friend groups during that time. When I got here it was quite obvious that those groups had been established. For the first time in as long as I can remember, I found myself in an environment where I had to be intentional about meeting people. Although I ended up making a lot of friends, I felt more that I was making them out of necessity, and not because I genuinely enjoyed their company. It goes without saying that I did not make much effort to maintain those relationships.
+                </p>
+                <p>
+                    I remember when we first met at the gym and had a brief interaction, I asked you and Rana if you could get the door when I came back from filling my water bottle. The truth is that there was probably enough water left, and I honestly wasn’t even that thirsty. But I knew that you opening the door would welcome conversation, and you guys seemed interesting to get to know. That intuition served me well. Over the past little bit that I’ve known you I’ve been surprised at your kindness and spontaneity - things I struggle to find in Singapore. I didn’t realize how much I just needed to fuck around and dress up as a Korean animated penguin for a day, and that this was how I would make friends.
+                </p>
+                <p>
+                    There’s a lot that can be said in this regard but I’ll keep it short: I’m incredibly glad I refilled my water bottle that day.
+                </p>
+                <p>
+                    Thank you for helping make this term so memorable for me. Keep in touch :)
+                </p>
+                <p>___</p>
+                <p>
+                    Yash.
+                </p>
+                <p>
+                    2023-11-30
+                </p>
+                <p>___</p>
+                <p>
+                    P.S.
+                </p>
+                <p>
+                    Hope you enjoy the break! Make sure to make up for all the time you couldn’t spend with family over covid.
+                </p>
+
+            </div>
+        </div>
+    )
+};
+
+export default Loopy;
